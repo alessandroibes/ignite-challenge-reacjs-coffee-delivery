3406aa40834a641be3faa0b06551141ff649dbea
alessandroibes <alessandro.ibes@gmail.com>
2024-06-25 17:31:16 -0300

    Criação da página Home

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from 'styled-components';
 
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
+import { Home } from './pages/Home';
 
 export function App() {
   return (
@@ -19,77 +20,7 @@ export function App() {
       </div>
 
       <main className="mainContent">
-        <section className="hero">
-          <div className="heroContent">
-            <div>
-              <div className="heroHeading">
-                <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-
-                <span>
-                  Com o Coffee Delivery você recebe seu café onde estiver, a
-                  qualquer hora
-                </span>
-              </div>
-
-              <div className="heroInfo">
-                <div>
-                  <span>Compra simples e segura</span>
-                </div>
-
-                <div>
-                  <span>Embalagem mantém o café intacto</span>
-                </div>
-
-                <div>
-                  <span>Entrega rápida e rastreada</span>
-                </div>
-
-                <div>
-                  <span>O café chega fresquinho até você</span>
-                </div>
-              </div>
-            </div>
-
-            <img src="" />
-          </div>
-        </section>
-
-        <section className="coffeeList">
-          <h2>Nossos cafés</h2>
-
-          <div>
-            <div className="cardContainer">
-              <img src=""></img>
-
-              <div className="cardTags">
-                <span>TRADICIONAL</span>
-              </div>
-
-              <h3 className="cardTitle">Expresso Tradicional</h3>
-
-              <span className="cardDescription">
-                O tradicional café feito com água quente e grãos moídos
-              </span>
-
-              <div className="cardControl">
-                <div className="cardPrice">
-                  <span>R$</span>
-                  <span>9,90</span>
-                </div>
-
-                <div className="cardOrder">
-                  <div className="cardQuantityInput">
-                    <button>decrement</button>
-                    <span>1</span>
-                    <button>increment</button>
-                  </div>
-
-                  <button>Carrinho</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <Home />
       </main>
     </ThemeProvider>
   );
