8810560611afd08cfb8c28135cd28199c5cd5b0a
alessandroibes <alessandro.ibes@gmail.com>
2024-07-01 09:17:15 -0300

    Página success

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,3 +1,53 @@
+import { CurrencyDollar, MapPin, Timer } from '@phosphor-icons/react';
+import { Container, Heading, Info, InfoContent, Order } from './styles';
+
 export function Success() {
-  return <div>Success</div>;
+  return (
+    <Container>
+      <Order>
+        <Heading>
+          <h2>Uhu! Pedido confirmado</h2>
+          <span>Agora é só aguardar que logo o café chegará até você</span>
+        </Heading>
+
+        <Info>
+          <InfoContent>
+            <div>
+              <MapPin size={32} />
+
+              <div>
+                <span>
+                  Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                </span>
+
+                <span>Farrapos - Porto Alegre, RS</span>
+              </div>
+            </div>
+
+            <div>
+              <Timer size={32} />
+
+              <div>
+                <span>Previsão de entrega</span>
+
+                <strong>20 min - 30 min</strong>
+              </div>
+            </div>
+
+            <div>
+              <CurrencyDollar size={32} />
+
+              <div>
+                <span>Pagamento na entrega</span>
+
+                <strong>Cartão de Crédito</strong>
+              </div>
+            </div>
+          </InfoContent>
+        </Info>
+      </Order>
+
+      <img src="/images/delivery.svg" alt="Pedido concluído" />
+    </Container>
+  );
 }
---
+++ src/pages/Success/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.main``;
+
+export const Order = styled.section``;
+
+export const Heading = styled.div``;
+
+export const Info = styled.div``;
+
+export const InfoContent = styled.div``;
